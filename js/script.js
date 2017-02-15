d6e064d540eb7b6c3fcfbc4b4cf61a83f8d1f3aa
Colin Shaw <xkidshaw@gmail.com>
2017-02-15 19:30:50 +0800

    working on chinese version

--- js/script.js
+++ js/script.js
@@ -3,39 +3,58 @@
 $(document).ready(function(){
 
 
-	function loadSlider(){
+	function loadShits(){
 		$('.bxslider').bxSlider();
+
+		// masonry
+		setTimeout(function() {
+			var container = document.querySelector('#container');
+			var msnry = new Masonry( container, {
+  				// options
+  				columnWidth: 5,
+  				itemSelector: '.item'
+  			});
+		},1000);
 	}
 
 
+	loadShits()
 
 
-	//fade page animation
+	// ajax load pages contents
+	$('.container a').click(function(e){
+		e.preventDefault();
+		var thisUrl = $(this).attr('href');
+		$("#loadContentHere").html('').load('/' +thisUrl+' #loadContentFrom', function(){
+			// alert( "Load was performed." );
+			loadShits();
+		});
 
-	$(".animsition").animsition({
+	});
 
-		inClass               :   'fade-in',
-		outClass              :   'fade-out',
-		inDuration            :    1500,
-		outDuration           :    800,
-		linkElement           :   '.animsition-link', 
-    	// e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-    	loading               :    true,
-    	loadingParentElement  :   'body', //animsition wrapper element
-    	loadingClass          :   'animsition-loading',
-    	unSupportCss          : [ 'animation-duration',
-    	'-webkit-animation-duration',
-    	'-o-animation-duration'
-    	],
-    	//"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser. 
-    	//The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+		//fade page animation
 
-    	overlay               :   false,
-
-    	overlayClass          :   'animsition-overlay-slide',
-    	overlayParentElement  :   'body'
-    });
-
+		$(".animsition").animsition({
+			inClass: 'fade-in',
+			outClass: 'fade-out',
+			inDuration: 1500,
+			outDuration: 800,
+			linkElement: '.animsition-link',// e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+			loading: true,
+    		loadingParentElement: 'body', //animsition wrapper element
+    		loadingClass: 'animsition-loading',
+    		loadingInner: '', // e.g '<img src="loading.svg" />'
+    		timeout: false,
+    		timeoutCountdown: 5000,
+    		onLoadEvent: true,
+    		browser: [ 'animation-duration', '-webkit-animation-duration'],
+    		// "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    		// The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    		overlay : false,
+    		overlayClass : 'animsition-overlay-slide',
+    		overlayParentElement : 'body',
+    		transition: function(url){ window.location.href = url; }
+    	});
 
 
 	//lang
@@ -51,14 +70,7 @@ $(document).ready(function(){
 	});
 
 
-	$('.container a').click(function(e){
-		e.preventDefault();
-		$("#loadContentHere").html('').load('/works/myTrends.html #loadContentFrom', function(){
-			alert( "Load was performed." );
-			loadSlider();
-		});
 
-});
 
 
 
@@ -86,17 +98,6 @@ $(document).ready(function(){
 
 
 
-
-
-	// masonry
-	setTimeout(function() {
-		var container = document.querySelector('#container');
-		var msnry = new Masonry( container, {
-  			// options
-  			columnWidth: 5,
-  			itemSelector: '.item'
-  		});
-	});
 
 
 
